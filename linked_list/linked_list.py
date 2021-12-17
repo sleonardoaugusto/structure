5c84377fd66bea392ae5c8a0ba6f551e5e69cd6e
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-12-17 03:38:57 -0300

    Linked list: prepend node

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -49,3 +49,13 @@ class LinkedList:
                 self.length -= 1
                 break
             node = node.next
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
