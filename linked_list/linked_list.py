4f382f9785118c8ac84a6829b5bf700c0706ae20
Leonardo Augusto <sleonardoaugusto@gmail.com>
2022-02-06 14:07:55 -0300

    Linked list: Practing and refactoring

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -8,79 +8,66 @@ class LinkedList:
     def __init__(self, value=None):
         self.head = None
         self.tail = None
-        self.length = None
+        self.length = 0
         self._create(value)
 
     def _create(self, value):
-        if value is None:
-            self.length = 0
-        else:
+        if value:
             node = Node(value)
             self.head = node
             self.tail = node
-            self.length = 1
-
-    def print_list(self):
-        node = self.head
-        while node:
-            print(node.value)
-            node = node.next
+            self.length += 1
 
     def append(self, value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-        else:
-            self.tail.next = new_node
-        self.tail = new_node
+        node = Node(value)
+        self.tail.next = node
+        self.tail = node
         self.length += 1
 
     def pop(self):
-        node = self.head
-        while node:
-            if node.next is None:
-                self.head = None
-                self.tail = None
-                self.length = 0
-                break
-            elif node.next.next is None:
-                node.next = None
-                self.tail = node
-                self.length -= 1
-                break
-            node = node.next
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+        elif self.length > 1:
+            temp = self.head
+            while temp.next.next is not None:
+                temp = temp.next
+            temp.next = None
+            self.tail = temp
+            self.length -= 1
 
     def prepend(self, value):
-        new_node = Node(value)
+        node = Node(value)
         if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
+            self.tail = node
+        temp = self.head
+        self.head = node
+        self.head.next = temp
         self.length += 1
 
     def pop_first(self):
-        temp = self.head
-        if temp:
-            self.head = temp.next
-        self.length -= 1
-        if self.length == 0:
-            self.tail = None
+        if self.length:
+            temp = self.head.next
+            self.head = temp
+            self.length -= 1
 
-    def get(self, idx):
-        temp = self.head
-        for _ in range(idx):
-            temp = temp.next
-        return temp
+    def get(self, index):
+        if self.length > index:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        else:
+            raise IndexError
 
     def set_value(self, idx, value):
         temp = self.get(idx)
         temp.value = value
 
     def insert(self, idx, value):
-        if idx < 0 or idx > self.length:
-            raise IndexOutOfRange
+        if idx < 0:
+            raise IndexError
         elif idx == 0:
             self.prepend(value)
         elif idx == self.length:
@@ -95,11 +82,9 @@ class LinkedList:
             self.length += 1
 
     def remove(self, idx):
-        if idx < 0 or idx > self.length - 1:
-            raise IndexOutOfRange
-        elif idx == 0:
-            self.pop_first()
-        elif idx == self.length - 1:
+        if idx < 0 or idx + 1 > self.length:
+            raise IndexError
+        if self.length == 1 or idx == self.length - 1:
             self.pop()
         else:
             temp = self.head
@@ -118,7 +103,3 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
-
-
-class IndexOutOfRange(BaseException):
-    ...
--- linked_list/tests/test_linked_list.py
+++ linked_list/tests/test_linked_list.py
@@ -1,185 +1,149 @@
 import pytest
 
-from linked_list.linked_list import LinkedList, IndexOutOfRange
+from linked_list.linked_list import LinkedList
 
 
-def test_init():
-    """Should init with node"""
-    linked_list = LinkedList(2)
-    assert linked_list.length == 1
-    assert linked_list.head.value == 2
-    assert linked_list.tail.value == 2
+def test_constructor():
+    ll = LinkedList(1)
+    assert ll.head.value == 1
+    assert ll.tail.value == 1
+    assert ll.head.next is None
+    assert ll.length == 1
 
 
-def test_init_without_value():
-    """Should init without node"""
-    linked_list = LinkedList()
-    assert linked_list.length == 0
-    assert linked_list.head is None
-    assert linked_list.tail is None
+def test_constructor_empty_value():
+    ll = LinkedList()
+    assert ll.head is None
+    assert ll.tail is None
+    assert ll.length == 0
 
 
 def test_append():
-    """Should append node"""
-    linked_list = LinkedList()
-    linked_list.append(2)
-    assert linked_list.length == 1
-    assert linked_list.head.value == 2
-    assert linked_list.tail.value == 2
+    ll = LinkedList(1)
+    ll.append(2)
+    assert ll.head.value == 1
+    assert ll.tail.value == 2
+    assert ll.head.next.value == 2
+    assert ll.length == 2
 
 
-def test_append_without_node():
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    assert linked_list.length == 2
-    assert linked_list.head.value == 1
-    assert linked_list.tail.value == 2
+def test_pop_with_one_item():
+    ll = LinkedList(1)
+    ll.pop()
+    assert ll.head is None
+    assert ll.tail is None
+    assert ll.length == 0
 
 
-def test_pop_with_one_node():
-    """Should remove the only node"""
-    linked_list = LinkedList(1)
-    linked_list.pop()
-    assert linked_list.length == 0
-    assert linked_list.head is None
-    assert linked_list.tail is None
-
-
-def test_pop_with_two_nodes():
-    """Should remove last node"""
-    linked_list = LinkedList(2)
-    linked_list.append(3)
-    linked_list.pop()
-    assert linked_list.length == 1
-    assert linked_list.head.value == 2
-    assert linked_list.tail.value == 2
-
-
-def test_prepend_without_length():
-    """Should add node if list is empty"""
-    linked_list = LinkedList()
-    linked_list.prepend(2)
-    assert linked_list.length == 1
-    assert linked_list.head.value == 2
-    assert linked_list.tail.value == 2
+def test_pop_with_two_items():
+    ll = LinkedList(1)
+    ll.append(2)
+    ll.pop()
+    assert ll.head.value == 1
+    assert ll.tail.value == 1
+    assert ll.head.next is None
+    assert ll.length == 1
 
 
 def test_prepend():
-    """Should add node if list is not empty"""
-    linked_list = LinkedList(2)
-    linked_list.prepend(3)
-    assert linked_list.length == 2
-    assert linked_list.head.value == 3
-    assert linked_list.tail.value == 2
+    ll = LinkedList(2)
+    ll.prepend(1)
+    assert ll.head.value == 1
+    assert ll.tail.value == 2
+    assert ll.length == 2
+    assert ll.head.next.value == 2
 
 
-def test_pop_first_without_length():
-    """Should remove the only node"""
-    linked_list = LinkedList(1)
-    linked_list.pop_first()
-    assert linked_list.length == 0
-    assert linked_list.head is None
-    assert linked_list.tail is None
+def test_get():
+    ll = LinkedList(1)
+    node = ll.get(0)
+    assert node.value == 1
 
 
-def test_pop_first():
-    """Should remove first node"""
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.pop_first()
-    assert linked_list.length == 1
-    assert linked_list.head.value == 2
-    assert linked_list.tail.value == 2
-
-
-def test_get_index():
-    """Should return value by index"""
-    linked_list = LinkedList(1)
-    assert linked_list.get(0).value == 1
+def test_get_out_of_range():
+    ll = LinkedList(1)
+    with pytest.raises(IndexError):
+        ll.get(1)
 
 
 def test_set_value():
-    """Should set new value by index"""
-    linked_list = LinkedList(1)
-    linked_list.set_value(0, 2)
-    assert linked_list.head.value == 2
+    ll = LinkedList(1)
+    ll.set_value(0, 2)
+    assert ll.get(0).value == 2
 
 
-def test_insert_out_of_range():
-    """Should raise an exception if index is out of range"""
-    linked_list = LinkedList(1)
-    with pytest.raises(IndexOutOfRange):
-        linked_list.insert(2, 2)
-
-
-def test_insert_on_the_head():
-    """Should insert item on the top of the list"""
-    linked_list = LinkedList(1)
-    linked_list.insert(0, 2)
-    assert linked_list.get(0).value == 2
-    assert linked_list.head.value == 2
+def test_insert_on_the_beginning():
+    ll = LinkedList(2)
+    ll.insert(0, 1)
+    assert ll.head.value == 1
+    assert ll.tail.value == 2
+    assert ll.head.next.value == 2
+    assert ll.length == 2
 
 
 def test_insert_on_the_middle():
-    """Should insert item on the middle of the list"""
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.insert(1, 3)
-    assert linked_list.get(1).value == 3
-    assert linked_list.get(1).next.value == 2
+    ll = LinkedList(1)
+    ll.append(3)
+    ll.insert(1, 2)
+    assert ll.head.value == 1
+    assert ll.head.next.value == 2
+    assert ll.head.next.next.value == 3
+    assert ll.length == 3
 
 
 def test_insert_on_the_end():
-    """Should insert item on the end of the list"""
-    linked_list = LinkedList(1)
-    linked_list.insert(1, 2)
-    assert linked_list.get(1).value == 2
-    assert linked_list.tail.value == 2
+    ll = LinkedList(1)
+    ll.append(2)
+    ll.insert(2, 3)
+    assert ll.head.value == 1
+    assert ll.head.next.value == 2
+    assert ll.head.next.next.value == 3
+    assert ll.tail.value == 3
+    assert ll.length == 3
 
 
 def test_remove_out_of_range():
-    """Should raise an exception if index is out of range"""
-    linked_list = LinkedList(1)
-    with pytest.raises(IndexOutOfRange):
-        linked_list.remove(1)
+    ll = LinkedList()
+    with pytest.raises(IndexError):
+        ll.remove(0)
 
 
-def test_remove_on_the_head():
-    """Should remove item by index on head"""
-    linked_list = LinkedList(1)
-    linked_list.remove(0)
-    assert linked_list.get(0) is None
-    assert linked_list.head is None
-    assert linked_list.tail is None
+def test_remove_from_the_beginning():
+    ll = LinkedList(1)
+    ll.remove(0)
+    assert ll.head is None
+    assert ll.tail is None
+    assert ll.length == 0
 
 
-def test_remove_on_the_middle():
-    """Should remove item by index on middle"""
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.remove(1)
-    assert linked_list.get(0).value == 1
-    assert linked_list.get(1).value == 3
+def test_remove_from_the_middle():
+    ll = LinkedList(1)
+    ll.append(2)
+    ll.append(3)
+    ll.remove(1)
+    assert ll.head.value == 1
+    assert ll.head.next.value == 3
+    assert ll.tail.value == 3
+    assert ll.length == 2
 
 
-def test_remove_on_the_end():
-    """Should remove item by index on end"""
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.remove(1)
-    assert linked_list.get(0).value == 1
-    assert linked_list.head.value == 1
-    assert linked_list.tail.value == 1
+def test_remove_from_the_end():
+    ll = LinkedList(1)
+    ll.append(2)
+    ll.append(3)
+    ll.remove(2)
+    assert ll.head.value == 1
+    assert ll.head.next.value == 2
+    assert ll.tail.value == 2
+    assert ll.length == 2
 
 
 def test_reverse():
-    """Should reverse list"""
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.reverse()
-    assert linked_list.head.value == 3
-    assert linked_list.tail.value == 1
-    assert linked_list.head.next.value == 2
-    assert linked_list.tail.next is None
+    ll = LinkedList(1)
+    ll.append(2)
+    ll.append(3)
+    ll.reverse()
+    assert ll.head.value == 3
+    assert ll.head.next.value == 2
+    assert ll.tail.value == 1
+    assert ll.length == 3
