00476f03084b29f943e5de2e71cff1d36d51b9b8
Leonardo Augusto <sleonardoaugusto@gmail.com>
2022-02-19 19:16:56 -0300

    Doubly Linked List: fix test names

--- doubly_linked_list/tests/test_doubly_linked_list.py
+++ doubly_linked_list/tests/test_doubly_linked_list.py
@@ -1,21 +1,21 @@
 from doubly_linked_list.doubly_linked_list import DoublyLinkedList
 
 
-def test_constructor_with_empty_value():
+def test_constructor_without_value():
     dll = DoublyLinkedList()
     assert dll.length == 0
     assert dll.head is None
     assert dll.tail is None
 
 
-def test_constructor_with_value():
+def test_constructor():
     dll = DoublyLinkedList(1)
     assert dll.length == 1
     assert dll.head.value == 1
     assert dll.tail.value == 1
 
 
-def test_append_without_length():
+def test_append_empty_list():
     dll = DoublyLinkedList()
     dll.append(1)
     assert dll.length == 1
@@ -25,7 +25,7 @@ def test_append_without_length():
     assert dll.tail.next is None
 
 
-def test_append_with_length():
+def test_append():
     dll = DoublyLinkedList(1)
     dll.append(2)
     assert dll.length == 2
